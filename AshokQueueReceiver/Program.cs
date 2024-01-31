af842cb05bf60615736042015e61f0bd253dfed2
Ashok Vemulapalli <ashk406@gmail.com>
2024-01-31 15:26:13 +0530

    Add project files.

---
+++ AshokQueueReceiver/Program.cs
@@ -0,0 +1,48 @@
+﻿using System.Threading.Tasks;
+using Azure.Messaging.ServiceBus;
+
+ServiceBusClient client;
+ServiceBusProcessor processor;
+
+var clientOptions = new ServiceBusClientOptions()
+{
+    TransportType = ServiceBusTransportType.AmqpWebSockets
+};
+
+client = new ServiceBusClient("Endpoint=sb://ashoknamespace.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=r06TAy7XaHW6T8pvIjUJCKdeyOt1juU69+ASbK381+Y=", clientOptions);
+processor = client.CreateProcessor("ashokqueue", new ServiceBusProcessorOptions());
+
+try
+{
+    processor.ProcessMessageAsync += MessageHandler;
+    processor.ProcessErrorAsync += ErrorHandler;
+
+    await processor.StartProcessingAsync();
+
+    Console.WriteLine("Wait for a minute and then press any key to end the processing");
+    Console.ReadKey();
+
+    Console.WriteLine("\nStopping the receiver...");
+    await processor.StopProcessingAsync();
+    Console.WriteLine("Stopped receiving messages");
+}
+finally
+{
+    await processor.DisposeAsync();
+    await client.DisposeAsync();
+}
+
+// handle received messages
+async Task MessageHandler(ProcessMessageEventArgs args)
+{
+    string body = args.Message.Body.ToString();
+    Console.WriteLine($"Received: {body}");
+    await args.CompleteMessageAsync(args.Message);
+}
+
+// handle any errors when receiving messages
+Task ErrorHandler(ProcessErrorEventArgs args)
+{
+    Console.WriteLine(args.Exception.ToString());
+    return Task.CompletedTask;
+}
---
+++ AshokQueueSender/Program.cs
@@ -0,0 +1,41 @@
+﻿using Azure.Messaging.ServiceBus;
+using Azure.Identity;
+
+ServiceBusClient client;
+ServiceBusSender sender;
+const int numOfMessages = 3;
+
+var clinentOptions = new ServiceBusClientOptions
+{
+    TransportType = ServiceBusTransportType.AmqpWebSockets
+};
+
+client = new ServiceBusClient("Endpoint=sb://ashoknamespace.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=r06TAy7XaHW6T8pvIjUJCKdeyOt1juU69+ASbK381+Y=", clinentOptions);
+sender = client.CreateSender("ashokqueue");
+
+//create a Batch
+using ServiceBusMessageBatch messageBatch = await sender.CreateMessageBatchAsync();
+for (int i = 1; i <= numOfMessages; i++)
+{
+    if (!messageBatch.TryAddMessage(new ServiceBusMessage($"Message {i}")))
+    {
+        throw new Exception($"This message {i} is too loarge to fit in the batch");
+    }
+}
+
+try
+{
+    await sender.SendMessagesAsync(messageBatch);
+    Console.WriteLine($"A batch of {numOfMessages} messages has been published to the queue.");
+}
+catch (Exception)
+{
+    throw;
+}
+finally
+{
+    await sender.DisposeAsync();
+    await client.DisposeAsync();
+}
+Console.WriteLine("Press any key to end the application");
+Console.ReadKey();
